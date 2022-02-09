143838ec17b666fec838e9472b5738da36d40812
sj0726 <123a556@naver.com>
2022-02-09 03:45:24 -0500

    json testing

--- Rising Stars/test.py
+++ Rising Stars/test.py
@@ -1,1 +1,23 @@
-print("Current Institution: Georgia Institute of Technology".split("Current Institution:"))
+import json
+
+f = open('data/MIT2021.json')
+data = json.load(f)
+
+# print(json.dumps(data, sort_keys=True))
+new = {'data': []}
+for x in data:
+    print(data[x])
+    temp = {
+            'name': x,
+            'institution:': data[x]['institution'],
+            'position': data[x]['position'],
+            'description': data[x]['description'],
+            'contact': data[x]['contact'],
+            'personal page': data[x]['personal page']
+    }
+    new['data'].append(temp)
+
+final = json.dumps(new, indent=4)
+with open("test.json", "w") as f: 
+    f.write(final)
+    f.close()
